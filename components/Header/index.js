9b8cc8deded41dfde86d2c24ebdbd9a759acdf91
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-07-20 11:48:57 -0500

    built out header component

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,21 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header() {
+    const headerDiv = document.createElement('div');
+    headerDiv.classList.add('header');
+
+    const dateSpan = document.createElement('span');
+    dateSpan.classList.add('date');
+    dateSpan.textContent = 'SJULY 20, 2019';
+    headerDiv.appendChild(dateSpan);
+
+    const headerH1 = document.createElemnt('h1');
+    headerH1.textContent = 'Lambda Times';
+    headerDiv.appendChild(headerH1);
+
+    const tempSpan = document.createElement('span');
+    tempSpan.classList.add('temp');
+    tempSpan.textContent = '98*';
+    headerDiv.appendChild(tempSpan);
+}
